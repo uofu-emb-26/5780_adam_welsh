62569591c30c94efb28082f1495ce5437b543f40
Adam Welsh <u1456456@utah.edu>
2026-03-04 16:40:02 -0700

    finished checkoff 1. setup adc config, calbr, start, etc. added led thresholds and read channel 10 adc to turn on led

--- lab6/Src/lab6.c
+++ lab6/Src/lab6.c
@@ -3,6 +3,9 @@
 
 void SystemClock_Config(void);
 
+void config_start_ADC(void);
+
+
 /**
   * @brief  The application entry point.
   * @retval int
@@ -16,9 +19,62 @@ int main(void)
 
   // beginning lab 6
 
+  // initializing led pins to output and starting clock
+  RCC->AHBENR |= (1<<19);
+
+
+  GPIO_InitTypeDef GPIO_LED_InitStruct = {GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_7 | GPIO_PIN_6,
+                              GPIO_MODE_OUTPUT_PP,
+                             GPIO_SPEED_FREQ_LOW,
+                               GPIO_NOPULL};
+  HAL_GPIO_Init(GPIOC, &GPIO_LED_InitStruct);
+
+  // initilaizng adc pin to addl fn
+  GPIOC->MODER |= 0x3;
+
+  // enabling adc clk
+  RCC->APB2ENR |= (1<<9);
+
+  // ADC config + start
+
+  //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+  config_start_ADC();
+
+  uint8_t read_val;
+
+  uint8_t thresh1 = 40;
+  uint8_t thresh2 = 100;
+  uint8_t thresh3 = 190;
+  uint8_t thresh4 = 230;
+
+  
+
   while (1)
   {
- 
+
+    read_val = ADC1->DR;
+
+    if(read_val > thresh4){
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+    }
+    else if(read_val > thresh3){
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+    }
+    else if(read_val > thresh2){
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+
+    }
+    else if(read_val > thresh1){
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+    }
+    else{
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
+    }
+
+    HAL_Delay(100);
   }
   return -1;
 }
@@ -57,6 +113,37 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 }
+
+void config_start_ADC(void){
+  // 8 bit res
+  ADC1->CFGR1 &= ~(1<<3);
+  ADC1->CFGR1 |= (1<<4);
+
+  // cont conversion mode
+  ADC1->CFGR1 |= (1<<13);
+
+  // disabling hardware triggers
+  ADC1->CFGR1 &= ~(0x3<<10);
+
+  // selecting channel
+  ADC1->CHSELR |= (1<<10);
+
+  // starting calibration
+  ADC1->CR |= (1<<31);
+
+  // wait for calibration to complete
+  while(ADC1->CR & (1<<31)){}
+
+  // enable adc
+  ADC1->CR |= (1<<0);
+
+  // wait for ADC ready - ready when adcrdy is high
+  while((ADC1->ISR & (1<<0)) == 0){}
+
+  // start conversion
+  ADC1->CR |= (1<<2);
+}
+
 
 /**
   * @brief  This function is executed in case of error occurrence.
